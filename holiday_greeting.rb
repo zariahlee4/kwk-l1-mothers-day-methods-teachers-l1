b123f2905831c46bdbd9cac454da79bdaeb71a0d
zariahlee4 <sneakiana@gmail.com>
2018-06-06 21:35:00 +0000

    automatically backed up by learn

---
+++ holiday_greeting.rb
@@ -0,0 +1,15 @@
+
+
+def name_length
+  puts"whats your name?"
+x= gets.chomp! .to_i
+if name_length =< 3
+  puts "your name is short"
+  elsif name_length =< 7
+  puts "your hellaaaaa average"
+else
+  puts"your name is longgggggggg"
+end
+
+
+name
